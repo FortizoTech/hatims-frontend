4962660fb7641aa96fcf337b29174a880411655c
Fortizo Technologies <fortizotechnologies1@gmail.com>
2026-03-17 19:55:32 +0000

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export const API_BASE_URL = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL.replace(/\/api\/?$/, '') : "http://localhost:5000";
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api"
+    baseURL: import.meta.env.VITE_API_URL || "https://hatims-backend-repo.onrender.com/api"
 })
 
 API.interceptors.request.use((req) => {
